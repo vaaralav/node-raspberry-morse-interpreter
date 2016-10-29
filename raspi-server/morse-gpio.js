9870d2f48f28f8e546732d8c6e81d9eb978b784f
Ville Vaarala <ville.l.vaarala@gmail.com>
2016-10-29 20:13:27 +0300

    Add GPIO blink morse code endpoint

--- raspi-server/morse-gpio.js
+++ raspi-server/morse-gpio.js
@@ -1,24 +1,42 @@
 var wpi = require('wiring-pi');
 
-const SHORT = 200;
-const LONG = 500;
+const TIME_UNIT = 150;
 
-const time = {
-  '.': SHORT,
-  '-': LONG,
-  ' ': LONG
-}
+const morseUnits = {
+  '.': {
+    'mode': wpi.HIGH,
+    'time': TIME_UNIT
+  },
+  '-': {
+    'mode': wpi.HIGH,
+    'time': 3 * TIME_UNIT
+  },
+  ' ': {
+    'mode': wpi.LOW,
+    'time': 2 * TIME_UNIT
+  }
+};
 
 // Use gpio pin numbering
 wpi.setup('gpio');
 
 module.exports.blinkMorseCode = function blinkMorseCode(morseCode, pin) {
-  wpi.pinMode(pin, 'OUTPUT');
-  morseCode.split("").map((c) => {
-    wpi.digitalWrite(pin, 'HIGH');
-    wpi.delay(time[c] || (2 * LONG));
-    wpi.digitalWrite(pin, 'LOW');
-    wpi.delay(SHORT);
+  wpi.pinMode(pin, wpi.OUTPUT);
+  morseCode.split('').map((c) => {
+    // Get unit. Default to space between words.
+    const unit = morseUnits[c] || {'mode': wpi.LOW, 'time': 6 * TIME_UNIT};
+    wpi.digitalWrite(pin, unit.mode);
+    wpi.delay(unit.time);
+    // Always add space between parts of the same letter == 1 unit
+    wpi.digitalWrite(pin, wpi.LOW);
+    wpi.delay(TIME_UNIT);
   });
+}
+
+module.exports.estimateTime = function estimateTime(morseCode) {
+  return morseCode.split('').reduce(function(totalTime, c) {
+    totalTime += morseUnits[c].time || 6 * TIME_UNIT;
+    totalTime += TIME_UNIT;
+  }, 0);
 }
 
